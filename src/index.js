75af95dcfba864bae5703f37dceb5c5b94c5fe95
AlexPirko <youzhny.foto@gmail.com>
2022-07-26 15:00:37 +0300

    done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
-module.exports = function reverse (n) {
+module.exports = function reverse(n) {
   
+    let reversed = 0;
+
+    let x = Math.abs(n);
+
+    while(x) {
+        reversed = reversed * 10 + x % 10;
+        x = Math.floor(x / 10);
+    }
+    
+    return reversed
 }
+
+
+
+
+
+
